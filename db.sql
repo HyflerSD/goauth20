3cb757053c290abbae43e5571e7da11b4fa21357
michael <michael@peoplelikesoftware.com>
2024-09-11 22:39:30 -0400

    this is the second initial i think idk bro

---
+++ db.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS oauth_clients;
+CREATE TABLE oauth_clients (
+	id INT AUTO_INCREMENT NOT NULL,
+	secret VARCHAR(128),
+	isActive BOOLEAN,
+	scopes VARCHAR(256),
+	PRIMARY KEY (`id`)
+);
+
+INSERT INTO oauth_client
+    (id, secret, isActive, scopes)
+VALUES
+    ('1', 'secret1', false, 'scope1,scope2,scope3'),
+    ('2', 'secret2', true, 'scope1,scope2,scope3'),
+    ('3', 'secret3', false, 'scope1,scope2,scope3'),
+    ('4', 'secret4', true, 'scope1,scope2,scope3');
+
+
+DROP TABLE IF EXISTS oauth_users;
+CREATE TABLE oauth_users (
+	id INT AUTO_INCREMENT NOT NULL,
+	email VARCHAR(128) NOT NULL,
+	password VARCHAR(128) NOT NULL,
+	PRIMARY KEY (`id`)
+
+);
+
+
+INSERT INTO oauth_users
+    (id, email, password)
+VALUES
+    ('1', 'm@gmail.com', 'kkksks'),
+    ('2', 's@gmail.com', 'kla;a;'),
+    ('3', 'j@gmail.com', 'p.;w92922');
+
+
+
--- main.go
+++ main.go
@@ -1,10 +1,114 @@
 package main
 
 import (
+	//"errors"
+	"database/sql"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+	"log"
+	"os"
 )
+
+type Auth interface {
+	Authorize(fd *os.File) error
+	Revoke(fd *os.File) error
+}
+
+type Creator interface {
+	Create(fd *os.File) error
+	Delete(fd *os.File) error
+}
+
+type User struct {
+	email    string
+	password string
+	id       int32
+}
+
+type TempAuthCodes struct {
+	clientID int32
+	userID   int32
+	scopes   []string
+	authCode string
+	expires  string
+}
+
+type OauthClient struct {
+	clientID     int32
+	clientSecret string
+	isActive     bool
+	scopes       []string
+	redirectURI  string
+}
+
+type OauthUser struct {
+	userID      int32
+	clientID    int32
+	accessToken string
+	scope       []string
+	expires     string
+}
+
+var db *sql.DB
 
 func main() {
 
-	fmt.Println("hello workssl")
+	cfg := mysql.Config{
+		User:   os.Getenv("GODBUSER"),
+		Passwd: os.Getenv("GODBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: os.Getenv("GOAUTHDB"),
+	}
+
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Println("connected!")
+}
+
+/** CLIENT STUFF*/
+func (a *OauthClient) Create(fd *os.File) bool {
+	return true
+}
+
+func (a *OauthClient) Authorize(fd *os.File) bool {
+
+	return true
+}
+
+func (a *OauthClient) Revoke(fd *os.File) bool {
+	return true
+}
+
+func (a *OauthClient) Delete(fd *os.File) bool {
+	return true
+}
+
+/** USER STUFF*/
+
+func (a *OauthUser) Authorize(fd *os.File) bool {
+	return true
+}
+
+func (a *OauthUser) Revoke(fd *os.File) bool {
+	return true
+}
+
+/** GENERAL SERVER NEEDS */
+
+func (a *OauthUser) GenerateToken() string {
+
+	token := uuid.New().String()
+
+	return token
 }
